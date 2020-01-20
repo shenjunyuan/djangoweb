e119005930e29da41dfbf010b1f10472192418fd
shenjun <h14736985@gmail.com>
2020-01-20 15:25:52 +0800

    make it better

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#   /
+
 STATIC_URL = '/static/'
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 STATICFILES_DIRS = (
     ('css', os.path.join(STATIC_ROOT, 'css').replace('\\', '/')),
     ('images', os.path.join(STATIC_ROOT, 'images').replace('\\', '/')),

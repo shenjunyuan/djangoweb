2b5a9cd735e09a24757a3ad1e0db8fb56aba180d
shenjun <h14736985@gmail.com>
2020-02-12 14:55:50 +0800

    bootstrap_train

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 from dj_static import Cling
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = Cling(get_wsgi_application())
+
